dab57ef51ad130391373f503f254985772e6473a
VETRIVEL <vetrivel.galaxy@gmail.com>
2021-10-07 13:09:26 +0300

    Mobile Screen Updated

--- js/swiperScript.js
+++ js/swiperScript.js
@@ -1,17 +1,8 @@
 var swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
+  effect: "cards",
   grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 40,
-    stretch: 0,
-    depth: 100,
-    modifier: 5,
-    slideShadows: true,
-  },
-  autoplay: {
-    delay: 2000,
-  },
+  // autoplay: {
+  //   delay: 2000,
+  // },
   loop: true,
 });
